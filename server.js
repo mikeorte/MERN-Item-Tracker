b8b2f632ff9b641bd779c0357672183577b59acf
mikeorte <cuttingblade123@gmail.com>
2024-06-27 15:40:51 -0400

    6/27/24 Initial Commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://mrortegamichael:03sSeZamago2CcRt@cluster0.2lzmtl8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+
+// Define Schema and Model
+const itemSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+// Root Route
+app.get("/", (req, res) => {
+  res.send("Welcome to the MERN Simple Project API");
+});
+
+// Get All Items
+app.get("/items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+// Create New Item
+app.post("/items", async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+// Update Item
+app.put("/items/:id", async (req, res) => {
+  const updatedItem = await Item.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(updatedItem);
+});
+
+// Delete Item
+app.delete("/items/:id", async (req, res) => {
+  await Item.findByIdAndDelete(req.params.id);
+  res.json({ message: "Item deleted" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
